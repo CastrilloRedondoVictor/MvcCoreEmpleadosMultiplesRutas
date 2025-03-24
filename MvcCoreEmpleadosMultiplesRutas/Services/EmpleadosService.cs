9e1cc5ad0999c7036736515f7b89d9701343f5e2
Víctor Castrillo Redondo <castrilloredondovictor@gmail.com>
2025-03-24 13:06:24 +0100

    final changes

--- MvcCoreEmpleadosMultiplesRutas/Services/EmpleadosService.cs
+++ MvcCoreEmpleadosMultiplesRutas/Services/EmpleadosService.cs
@@ -17,66 +17,36 @@ namespace MvcCoreEmpleadosMultiplesRutas.Services
 
         public async Task<List<Empleado>> GetEmpleadosAsync()
         {
-            List<Empleado> empleados = new List<Empleado>();
-            using (HttpClient client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(ApiUrl);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(Header);
-                HttpResponseMessage response = await client.GetAsync("api/empleados");
-                if (response.IsSuccessStatusCode)
-                {
-                    empleados = await response.Content.ReadAsAsync<List<Empleado>>();
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            return empleados;
+            return await CallApiAsync<List<Empleado>>("api/empleados");
         }
 
         public async Task<List<string>> GetOficiosAsync()
         {
-            List<string> oficios = new List<string>();
-            using (HttpClient client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(ApiUrl);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(Header);
-                HttpResponseMessage response = await client.GetAsync("api/empleados/oficios");
-                if (response.IsSuccessStatusCode)
-                {
-                    oficios = await response.Content.ReadAsAsync<List<string>>();
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            return oficios;
+            return await CallApiAsync<List<string>>("api/empleados/oficios");
 
         }
 
         public async Task<List<Empleado>> GetEmpleadosOficioAsync(string oficio)
         {
-            List<Empleado> empleados = new List<Empleado>();
+            return await CallApiAsync<List<Empleado>>($"api/empleados/oficio/{oficio}");
+        }
+
+
+        private async Task<T> CallApiAsync<T>(string url)
+        {
+            T result = default(T);
             using (HttpClient client = new HttpClient())
             {
                 client.BaseAddress = new Uri(ApiUrl);
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(Header);
-                HttpResponseMessage response = await client.GetAsync($"api/empleados/oficio/{oficio}");
+                HttpResponseMessage response = await client.GetAsync(url);
                 if (response.IsSuccessStatusCode)
                 {
-                    empleados = await response.Content.ReadAsAsync<List<Empleado>>();
-                }
-                else
-                {
-                    return null;
+                    result = await response.Content.ReadAsAsync<T>();
                 }
             }
-            return empleados;
+            return result;
         }
     }
 }
